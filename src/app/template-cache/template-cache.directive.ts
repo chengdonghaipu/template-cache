01e880ad4a1e353da1c4789ab47c9ff0f6d8a09a
chendong <1296942311@qq.com>
2023-08-15 01:31:41 +0800

    feat(cache): 初步实现缓存指令 支持缓存

--- src/app/template-cache/template-cache.directive.ts
+++ src/app/template-cache/template-cache.directive.ts
@@ -4,9 +4,63 @@ import {Directive, EmbeddedViewRef, Input, TemplateRef, ViewContainerRef} from '
   selector: '[appCache]'
 })
 export class TemplateCacheDirective<T = unknown> {
+  private _context: CacheContext<T> = new CacheContext<T>();
+  private _thenTemplateRef: TemplateRef<CacheContext<T>>|null = null;
+  private _elseTemplateRef: TemplateRef<CacheContext<T>>|null = null;
+  private _thenViewRef: EmbeddedViewRef<CacheContext<T>>|null = null;
+  private _elseViewRef: EmbeddedViewRef<CacheContext<T>>|null = null;
 
+  @Input()
+  set appCache(condition: T) {
+    this._context.$implicit = condition;
+    this._updateView();
+  }
+
+  @Input()
+  set appCacheThen(templateRef: TemplateRef<CacheContext<T>>|null) {
+    assertTemplate('appCacheThen', templateRef);
+    this._thenTemplateRef = templateRef;
+    this._thenViewRef = null;  // clear previous view if any.
+    this._updateView();
+  }
+
+  @Input()
+  set appCacheElse(templateRef: TemplateRef<CacheContext<T>>|null) {
+    assertTemplate('appCacheElse', templateRef);
+    this._elseTemplateRef = templateRef;
+    this._elseViewRef = null;  // clear previous view if any.
+    this._updateView();
+  }
+
+  private _updateView() {
+    if (this._context.$implicit) {
+      if (!this._thenViewRef) {
+        this._viewContainer.detach()
+        // this._elseViewRef = null;
+        if (this._thenTemplateRef) {
+          this._thenViewRef =
+            this._viewContainer.createEmbeddedView(this._thenTemplateRef, this._context);
+        }
+      } else  {
+        this._viewContainer.detach()
+        this._viewContainer.insert(this._thenViewRef, 0)
+      }
+    } else {
+      if (!this._elseViewRef) {
+        this._viewContainer.detach()
+        if (this._elseTemplateRef) {
+          this._elseViewRef =
+            this._viewContainer.createEmbeddedView(this._elseTemplateRef, this._context);
+        }
+      } else  {
+        this._viewContainer.detach()
+        this._viewContainer.insert(this._elseViewRef)
+      }
+    }
+  }
   constructor(templateRef: TemplateRef<any>,
               private readonly _viewContainer: ViewContainerRef) {
+    this._thenTemplateRef = templateRef
   }
 
 }
