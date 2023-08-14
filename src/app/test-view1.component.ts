0f4431139d6ca448b6ba9891211f6978f522cc5b
chendong <1296942311@qq.com>
2023-08-15 01:35:46 +0800

    feat(test): 测试状态保存

--- src/app/test-view1.component.ts
+++ src/app/test-view1.component.ts
@@ -4,13 +4,19 @@ import {Component, DoCheck} from '@angular/core';
   selector: 'app-test-view1',
   template: `
     <p>
-      test-view1 works!
+      test-view1 works! {{count}}
     </p>
+    <button (click)="add()">add</button>
   `,
   styles: [
   ]
 })
 export class TestView1Component implements DoCheck {
+  count = 0
+
+  add() {
+    this.count ++;
+  }
   constructor() {
     console.log('TestView1Component');
     setInterval(() => {}, 1000)
